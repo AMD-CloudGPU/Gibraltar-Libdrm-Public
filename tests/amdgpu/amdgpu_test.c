fd244e6ea4a71475cfd12544ae83e48d897ed940
Flora Cui <flora.cui@amd.com>
2019-03-06 09:49:04 +0800

    tests/amdgpu: add deadlock test for sdma
    deadlock test for sdma will cause gpu recoverty.
    disable the test for now until GPU reset recovery could survive at least
    1000 times test.
    
    v2: add modprobe parameter
    
    Change-Id: I9adac63c62db22107345eddb30e7d81a1bda838c
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-and-tested-by: Evan Quan <evan.quan@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>

--- tests/amdgpu/amdgpu_test.c
+++ tests/amdgpu/amdgpu_test.c
@@ -427,6 +427,10 @@ static void amdgpu_disable_suites()
 		fprintf(stderr, "test deactivation failed - %s\n", CU_get_error_msg());
 
 	if (amdgpu_set_test_active(DEADLOCK_TESTS_STR,
+			"sdma ring block test (set amdgpu.lockup_timeout=50)", CU_FALSE))
+		fprintf(stderr, "test deactivation failed - %s\n", CU_get_error_msg());
+
+	if (amdgpu_set_test_active(DEADLOCK_TESTS_STR,
 			"illegal reg access test", CU_FALSE))
 		fprintf(stderr, "test deactivation failed - %s\n", CU_get_error_msg());
 
